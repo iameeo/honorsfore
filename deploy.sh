9adb5ac3e21a0fcf92bfa6f78305e302a6fb2cff
jaeho.lee <jaeho.lee@barunn.net>
2023-10-13 09:37:59 +0900

    deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 mv mysite _mysite
-git clone https://github.com/iameeo/honorsfore.git
-mv /home/honorsfore/honorsfore/mysite/ /home/honorsfore/
-rm -rf honorsfore/
+git clone https://github.com/iameeo/pythonanywhere_honorsfore.git
+mv /home/honorsfore/pythonanywhere_honorsfore/mysite/ /home/honorsfore/
+rm -rf pythonanywhere_honorsfore/
 rm -r _mysite/
