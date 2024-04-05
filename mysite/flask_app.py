6688bcbfc02df346e5c18650272f077347f152b2
jaeho.lee <jaeho.lee@barunn.net>
2024-04-05 17:02:41 +0900

    초기 셋팅 모바일 페이지 분리

--- mysite/flask_app.py
+++ mysite/flask_app.py
@@ -1,14 +1,18 @@
 from flask import Flask
 from flask import render_template, redirect, url_for, request
+from flask_mobility import Mobility
+from flask_mobility.decorators import mobile_template
 import requests
 import time
 
 app = Flask(__name__)
+Mobility(app)
 
 @app.route('/')
-def Index():
+@mobile_template("{m/}index.html")
+def Index(template):
     path = 'Index'
-    return render_template('Index.html', path=path)
+    return render_template(template, path=path)
     
 def slackMessage(text):
     url = 'https://hooks.slack.com/services/TMGE25VGT/B05GZRKLRU1/'
