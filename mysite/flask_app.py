06874888cb6415c9581f924b4bf73604b8befddf
jaeho.lee <jaeho.lee@barunn.net>
2024-07-03 10:22:22 +0900

    업로드 경로 변경

--- mysite/flask_app.py
+++ mysite/flask_app.py
@@ -7,7 +7,7 @@ from http import HTTPStatus
 app = Flask(__name__)
 Mobility(app)
 
-UPLOAD_FOLDER = os.getcwd() + '/mysite/static/upload'  # 절대 파일 경로
+UPLOAD_FOLDER = os.getcwd() + '/upload'  # 절대 파일 경로
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 
 @app.route('/')
